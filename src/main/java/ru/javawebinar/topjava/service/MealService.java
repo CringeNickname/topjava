d06c84683f85c6e19c11bc1bed92fa045f76056c
Justwantedtosavemyawfulcode <youjustalittlechicken@yandex.ru>
2022-11-12 19:23:39 +0300

    HW02 done probably

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,9 +1,42 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.stereotype.Service;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
 
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@Service
 public class MealService {
 
     private MealRepository repository;
 
+    public MealService(MealRepository repository) {
+        this.repository = repository;
+    }
+
+    public Meal create(Meal meal, int userId) {
+        return repository.save(meal, userId);
+    }
+
+    public void delete(int id, int userId) {
+        boolean deleted = repository.delete(id, userId);
+        if (!deleted) throw new NotFoundException("Not deleted!");
+    }
+
+    public Meal get(int id, int userId) {
+        Meal meal = repository.get(id, userId);
+        if (meal == null) throw new NotFoundException("Not found!");
+        else return meal;
+    }
+
+    public List<Meal> getAll(int userId) {
+        return new CopyOnWriteArrayList<>(repository.getAll(userId));
+    }
+
+    public void update(Meal meal, int userID) {
+        repository.save(meal, userID);
+    }
 }
--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,8 +1,54 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.to.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
 
+import java.util.List;
+
+@Controller
 public class MealRestController {
+    protected final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
     private MealService service;
+
+
+    public List<MealTo> getAll(int userId) {
+        log.info("getAll");
+
+        List<Meal> mealList = service.getAll(userId);
+        return MealsUtil.getTos(mealList, MealsUtil.DEFAULT_CALORIES_PER_DAY);
+    }
+
+    public Meal get(int id, int userId) {
+        log.info("get");
+
+        return service.get(id, userId);
+    }
+
+    public Meal create(Meal meal, int userId) {
+        log.info("create");
+
+        return service.create(meal, userId);
+    }
+
+    public void delete(int id, int userId) {
+        log.info("delete");
+
+        service.delete(id, userId);
+    }
+
+    public void update(Meal meal, int userId) {
+        log.info("update");
+
+        service.update(meal, userId);
+    }
+
 
 }
